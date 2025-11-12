a551be571577d55be2d283dec012517447af7112
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-11-12 15:52:01 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,10 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    pi_over_4_sum = 0.0
+
+    for k in range(n_terms):
+        term = ((-1)**k) / (2 * k + 1)
+        pi_over_4_sum += term
+
+pi_approximation = 4 * pi_over_4_sum
+
+return pi_approximation
